0f1ac42b00692db29c49a39fd88d35c06d222141
DanielSoloviov <71178139+DanielSoloviov@users.noreply.github.com>
2021-02-14 11:06:05 +0300

    Additional commit

--- test/Program.cs
+++ test/Program.cs
@@ -13,6 +13,7 @@ namespace test
         static void Main(string[] args)
         {
             Console.WriteLine("This is test program for Git!");
+            Console.WriteLine("This is an addition to the 1st version of this test!");
             Console.ReadLine();
         }
         
