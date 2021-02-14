55d44e5091d410a4f1c009e2243959e2d7d95090
DanielSoloviov <71178139+DanielSoloviov@users.noreply.github.com>
2021-02-14 10:56:12 +0300

    Test commit

---
+++ test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace test
+{
+    class Program
+    {
+       
+  
+       
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is test program for Git!");
+            Console.ReadLine();
+        }
+        
+        
+    }
+}
